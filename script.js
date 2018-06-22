cff118c6fe3dd1ec2f4e1288297a284afc9a9e33
petecoj <petecojohns@gmail.com>
2018-06-22 16:30:30 -0500

    Added handle submit function and add employee function.  the first function puts the input values into variable and the second appends them to a table on the DOM

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 $(document).ready(readyNow);
-function readyNow(){
+
+function readyNow() {
+    console.log('ready');
+    addClickHandlers();
 
 
-    
+
+}
+
+function addClickHandlers() {
+    //on click of button, submit new employee
+    $('#submit').on('click', handleSubmit);
+
+}
+
+function handleSubmit() {
+    //
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let numberID = $('#numberID').val();
+    let jobTitle = $('#jobTitle').val();
+    let annualSalary = $('#annualSalary').val();
+
+    addEmployee(firstName, lastName, numberID, jobTitle, annualSalary);
+
+}
+function addEmployee(firstName, lastName, numberID, jobTitle, annualSalary){
+    let $row = $('<tr></tr>')// creating a new row not yet on the DOM
+    //appending the data from the inputs into the row, also not yet on the DOM
+    $row.append(`<td>${firstName}</td>`);
+    $row.append(`<td>${lastName}</td>`);
+    $row.append(`<td>${numberID}</td>`);
+    $row.append(`<td>${jobTitle}</td>`);
+    $row.append(`<td>${annualSalary}</td>`);
+    $row.append(`<td><button class = "deleteBtn">DELETE</button></td>`)
+
+    // append the data to the actual DOM
+    $('#employeeChart').append($row);
+
 }
